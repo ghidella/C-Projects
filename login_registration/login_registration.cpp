1f011709bbf39d92f4d37ebfc123d2e9ee60a80a
Ghidella <raphaelfghidella@gmail.com>
2023-03-29 11:15:15 -0300

    v0.1

--- login_registration/login_registration.cpp
+++ login_registration/login_registration.cpp
@@ -28,15 +28,17 @@ bool verify(std::vector<User> storage, std::string login, std::string psswrd)
 int main()
 {
     std::vector<User> storage;
+    int option;
     while (true)
     {
         int option;
-        std::cout << "(2) Login" << '\n'
-                  << "(1) Register" << std::endl;
+        std::cout << "(1) Login" << '\n'
+                  << "(2) Register" << '\n'
+                  << "(3) Exit" << std::endl;
         std::cin >> option;
         std::cin.ignore();
         std::system("clear");
-        if (option == 1)
+        if (option == 2)
         {
             User user;
             std::string login, psswrd;
@@ -46,20 +48,48 @@ int main()
             std::getline(std::cin, psswrd);
             registration(&user, login, psswrd);
             storage.push_back(user);
+            std::system("clear");
         }
-        else if (option == 2)
+        else if (option == 1)
         {
+            int i = 0;
             while (true)
             {
                 std::string login, psswrd;
+                if (i == 3)
+                {
+                    std::cout << "You failed 3 times, try again later! " << std::endl;
+                    break;
+                }
                 std::cout << "Enter username: ";
                 std::getline(std::cin, login);
                 std::cout << "Enter password: ";
                 std::getline(std::cin, psswrd);
-                if(verify(storage, login, psswrd) == true) break;
-                std::system("clear");    
-                std::cout<<"Username or password incorrect, please try again !"<<std::endl;           
+                if (verify(storage, login, psswrd) == true)
+                {
+                    std::system("clear");
+                    std::cout << "You are now logged" << std::endl;
+                    int option2;
+                    std::cout << "(1)Logout" << std::endl;
+                    std::cin >> option2;
+                    if (option2 == 1){
+                        std::system("clear");
+                        break;
+                    }
+                    std::cout << "Invalid option, please try again!" << std::endl;
+                }
+                std::system("clear");
+                std::cout << "Username or password incorrect, please try again!" << std::endl;
+                i++;
             }
+        }
+        else if (option == 3)
+        {
+            break;
+        }
+        else
+        {
+            std::cout << "Invalid option, please try again !" << std::endl;
         }
     }
 
